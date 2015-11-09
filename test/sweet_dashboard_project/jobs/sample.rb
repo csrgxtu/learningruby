061166dd466cb5f5b99112f27bbbd5133bf34124
archer <1246506786@qq.com>
2015-11-08 23:44:04 -0500

    modify the home page to use our own widget

--- test/sweet_dashboard_project/jobs/sample.rb
+++ test/sweet_dashboard_project/jobs/sample.rb
@@ -2,14 +2,15 @@ current_valuation = 0
 current_karma = 0
 
 SCHEDULER.every '5s' do
-  print("Runing ...")
-  last_valuation = current_valuation
-  last_karma     = current_karma
-  current_valuation = rand(100)
-  current_karma     = rand(200000)
+  # last_valuation = current_valuation
+  # last_karma     = current_karma
+  # current_valuation = rand(100)
+  # current_karma     = rand(200000)
 
-  send_event('valuation', { current: current_valuation, last: last_valuation })
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
-  send_event('welcome', { text: rand(100).to_s })
+  # send_event('valuation', { current: current_valuation, last: last_valuation })
+  # send_event('karma', { current: current_karma, last: last_karma })
+  # send_event('synergy',   { value: rand(100) })
+  # send_event('welcome', { text: rand(100).to_s })
+  send_event('bookkindsnum', { text: rand(100)})
+  send_event('onlineusers', { text: rand(100)})
 end
