7b40fc005f4c7d20d98671738e1e97274a7b7771
archer <1246506786@qq.com>
2015-11-11 23:01:35 -0500

    final

--- test/sweet_dashboard_project/jobs/sample.rb
+++ test/sweet_dashboard_project/jobs/sample.rb
@@ -1,7 +1,17 @@
-current_valuation = 0
-current_karma = 0
+#!/usr/bin/env ruby
+# encoding: utf-8
+require 'unirest'
+require 'json'
+
+# current_valuation = 0
+# current_karma = 0
 
 SCHEDULER.every '2s' do
+  userUrl = "http://dev.yuedu.io:21010/beautilfulreading/user/count"
+  libUrl = "http://dev.yuedu.io:21010/beautilfulreading/library/count"
+  userResponse = Unirest.get userUrl, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
+  libResponse = Unirest.get libUrl, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
+  # print(userResponse.body['data'], libResponse.body['data'])
   # response = Unirest.get "http://serverip:port/gps"
   # last_valuation = current_valuation
   # last_karma     = current_karma
@@ -10,8 +20,8 @@ SCHEDULER.every '2s' do
 
   # send_event('valuation', { current: current_valuation, last: last_valuation })
   # send_event('karma', { current: current_karma, last: last_karma })
-  # send_event('synergy',   { value: rand(100) })
+  send_event('synergy',   { value: rand(100) })
   # send_event('welcome', { text: rand(100).to_s })
-  send_event('bookkindsnum', { text: rand(100)})
-  send_event('onlineusers', { text: rand(100)})
+  send_event('bookkindsnum', { text: userResponse.body['data'] + rand(10)})
+  send_event('onlineusers', { text: libResponse.body['data'] + rand(50)})
 end
