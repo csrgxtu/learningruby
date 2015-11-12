4bf6e2c1b31dc00a9fb5f3d6f94023d7d6430b92
archer <1246506786@qq.com>
2015-11-12 07:13:17 -0500

    feed the dash ui with data from backend

--- test/sweet_dashboard_project/jobs/sample.rb
+++ test/sweet_dashboard_project/jobs/sample.rb
@@ -7,10 +7,13 @@ require 'json'
 # current_karma = 0
 
 SCHEDULER.every '2s' do
-  userUrl = "http://dev.yuedu.io:21010/beautilfulreading/user/count"
-  libUrl = "http://dev.yuedu.io:21010/beautilfulreading/library/count"
-  userResponse = Unirest.get userUrl, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
-  libResponse = Unirest.get libUrl, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
+  # userUrl = "http://dev.yuedu.io:21010/beautilfulreading/user/count"
+  # libUrl = "http://dev.yuedu.io:21010/beautilfulreading/library/count"
+  # userResponse = Unirest.get userUrl, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
+  # libResponse = Unirest.get libUrl, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
+  url = "https://api_rio.beautifulreading.com/beautilfulreading/admin/statistics/main"
+  response = Unirest.get url, auth:{:user=>"loser", :password=>"england"}, headers:{ "Accept" => "application/json" }
+  puts(response.body['data'])
   # print(userResponse.body['data'], libResponse.body['data'])
   # response = Unirest.get "http://serverip:port/gps"
   # last_valuation = current_valuation
@@ -20,8 +23,12 @@ SCHEDULER.every '2s' do
 
   # send_event('valuation', { current: current_valuation, last: last_valuation })
   # send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
+  # send_event('synergy',   { value: rand(100) })
   # send_event('welcome', { text: rand(100).to_s })
-  send_event('bookkindsnum', { text: userResponse.body['data'] + rand(10)})
-  send_event('onlineusers', { text: libResponse.body['data'] + rand(50)})
+  send_event('books', { text: response.body['data']['library_count']})
+  send_event('users', { text: response.body['data']['online_count']})
+  send_event('booklists', { text: rand(50)})
+  send_event('reviews', { text: response.body['data']['comment_count']})
+  send_event('thumbups', { text: response.body['data']['favour_count']})
+  
 end
